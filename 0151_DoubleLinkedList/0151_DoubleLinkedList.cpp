5de2ec4f5019d37c8f5f4b299f9e9066fe97bb49
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-02 21:00:38 +0700

    Menghapus Memori Node yang Ditandai

--- 0151_DoubleLinkedList/0151_DoubleLinkedList.cpp
+++ 0151_DoubleLinkedList/0151_DoubleLinkedList.cpp
@@ -116,6 +116,10 @@ void deleteNode() {
 		}
 	}
 
+	// Release the memory of the node marked as current
+	delete current;
+	cout << "\x1b[32mRecord with roll number" << rollNo << "deleted\x1b[0m" << endl;
+
 }
 
 
