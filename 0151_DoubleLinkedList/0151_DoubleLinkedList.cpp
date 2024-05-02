37b07fb498a65b5296d5996b26fce49362d7f686
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-02 20:42:27 +0700

    Sisipkan Node Baru ke dalam Daftar

--- 0151_DoubleLinkedList/0151_DoubleLinkedList.cpp
+++ 0151_DoubleLinkedList/0151_DoubleLinkedList.cpp
@@ -18,4 +18,5 @@ void addNode() {
 	cin >> newNode->noMhs;									//Assign value to the data field of the new node
 	cout << "\nEnter the name of the student : ";
 	cin >> newNode->name;									//Assign value to the data field of the new node
+
 }
