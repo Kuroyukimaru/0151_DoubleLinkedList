ecdb1f741089a4c407a946773cb88da289c36bfa
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-02 20:59:08 +0700

    Node yang Akan Dihapus adalah Node Pertama

--- 0151_DoubleLinkedList/0151_DoubleLinkedList.cpp
+++ 0151_DoubleLinkedList/0151_DoubleLinkedList.cpp
@@ -101,6 +101,14 @@ void deleteNode() {
 		return;
 	}
 
+	// Node to be deleted is the first node
+	if (current == START) {
+		START = START->next;	// Step 2: update the START pointer
+		if (START != NULL) {
+			START->prev = NULL;	// Step
+		}
+	}
+
 }
 
 
